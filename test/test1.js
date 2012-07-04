f241668b1037b6fb14f37b9cf23785cecfe344ff
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-07-04 11:35:01 +0300

    Remove test from test1
    Assuming text and font is different between systems

--- test/test1.js
+++ test/test1.js
@@ -3,17 +3,9 @@ var Canvas = require('..').Canvas;
 module.exports = function() {
   var canvas = new Canvas(200, 200);
   var ctx = canvas.getContext('2d');
-
-  ctx.font = '30px Impact';
-  ctx.rotate(.1);
-  ctx.fillText("Awesome!", 50, 100);
-
-  var te = ctx.measureText('Awesome!');
-  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + te.width, 102);
-  ctx.stroke();
-
+  
+  ctx.fillStyle = 'red';
+  ctx.fillRect(10, 10, 50, 50);
+  
   return canvas;
 };
